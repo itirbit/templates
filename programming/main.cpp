d757962b80061898256e12c33e6cafc24147e2b4
itirbit <twmw@mail.ru>
2021-02-13 08:13:53 +0000

    is prime c++11

--- programming/main.cpp
+++ programming/main.cpp
@@ -1,7 +1,10 @@
-#include "isprime.hpp"
+#include "isprime11.hpp"
 #include <iostream>
 
 int main()
 {
-	std::cout << "is prime 9 " << IsPrime<9>::value << "\n";
+	std::cout << "is prime 9 " << isPrime(9) << "\n";
+	std::cout << "is prime 11 " << isPrime(11) << "\n";
+	std::cout << "is prime 49 " << isPrime(49) << "\n";
+	std::cout << "is prime 83 " << isPrime(83) << "\n";
 }

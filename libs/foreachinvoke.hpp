97da471f6b7e880d4b6f5f914d00fa2005341168
itirbit <twmw@mail.ru>
2021-02-19 10:16:51 +0000

    non void is same

--- libs/foreachinvoke.hpp
+++ libs/foreachinvoke.hpp
@@ -14,5 +14,13 @@ void foreach(Iter current, Iter end, Callable op, Args const& ... args)
 template <typename Callable, typename... Args>
 decltype(auto) call(Callable& op, Args&& ... args)
 {
-	return std::invoke(std::forward<Callable>(op), std::forward<Args>(args)...);
+	if constexpr(std::is_same_v<std::invoke_result_t<Callable, Args...>,void>)
+	{
+		return std::invoke(std::forward<Callable>(op), std::forward<Args>(args)...);
+	}
+	else
+	{
+		decltype(auto) ret{std::invoke(std::forward<Callable>(op), std::forward<Args>(args)...)};
+		return ret;
+	}
 }
